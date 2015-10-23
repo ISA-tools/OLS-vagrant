ac5af858475f265572415e0032bcdaa0f63b14f1
David Johnson <david.johnson@oerc.ox.ac.uk>
2015-10-23 19:49:29 +0100

    Initial commit. Installs prereqs

---
+++ puppet/manifests/classes/init.pp
@@ -0,0 +1,18 @@
+class init {
+
+    group { "puppet":
+        ensure => "present",
+    }
+
+    # Update the system
+    exec { "update-apt":
+        command => "sudo apt-get update",
+    }
+
+    # Install the first set of dependencies from apt
+    package {
+        ["mongodb", "maven", "solr-jetty", "tomcat7", "git"]:
+        ensure => installed,
+        require => Exec['update-apt'] # The system update needs to run first
+    }
+}
